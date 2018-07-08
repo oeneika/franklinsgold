15894a31007c28fd1e6f4946949678a1c0f3da9c
Alexander De Azevedo <alexander_deazevedo@hotmail.com>
2018-07-08 19:46:36 -0400

    recuperando el modelo de origen de greg

--- framework/app/models/Origen.php
+++ framework/app/models/Origen.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * This file is part of the Ocrend Framewok 3 package.
  *
@@ -8,9 +7,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 namespace app\models;
-
 use app\models as Model;
 use Ocrend\Kernel\Helpers as Helper;
 use Ocrend\Kernel\Models\Models;
@@ -18,34 +15,90 @@ use Ocrend\Kernel\Models\IModels;
 use Ocrend\Kernel\Models\ModelsException;
 use Ocrend\Kernel\Models\Traits\DBModel;
 use Ocrend\Kernel\Router\IRouter;
-
 /**
  * Modelo Origen
  */
 class Origen extends Models implements IModels {
     use DBModel;
-
+    private $nombre;
     /**
-     * Respuesta generada por defecto para el endpoint
+     * Revisa errores en el formulario
+     * 
+     * @return exception
+    */ 
+    private function errors(bool $edit = false){
+        global $http;
+        $this->nombre = $http->request->get('nombre');
+        
+        # Verificar que no están vacíos
+        if (Helper\Functions::e($this->nombre)) {
+            throw new ModelsException('Debe introducir un nombre.');
+        }
+        if($this->diametro<0 or $this->espesor<0 or $this->composicion<0 or $this->peso<0 ){
+            throw new ModelsException('El nombre del origen ya está en uso.');
+        }
+    }
+    /**
+     * Agrega usuarios 
      * 
      * @return array
     */ 
-    public function foo() : array {
+    public function add() : array {
         try {
-            global $http;
-                    
-            return array('success' => 0, 'message' => 'Funcionando');
+            #Revisa errores del formulario
+            $this->errors();
+            # Registrar al usuario
+            $id_origen =  $this->db->insert('moneda',array(
+                'nombre' => $this->nombre
+            ));
+            return array('success' => 1, 'message' => 'Origen creado con éxito!');
         } catch(ModelsException $e) {
             return array('success' => 0, 'message' => $e->getMessage());
         }
     }
-
-
+    /**
+     * Edita usuarios 
+     * 
+     * @return array
+    */ 
+    public function edit() : array {
+        try {
+            global $http;
+            $id = $http->request->get('id_origen');
+            $this->errors(true);
+            $data = array(
+                'nombre' => $this->nombre
+            );
+            #Edita una moneda
+            $this->db->update('origen',$data,"id_origen = '$id'",'1');
+            return array('success' => 1, 'message' => 'Origen editado con éxito!');
+        } catch(ModelsException $e) {
+            return array('success' => 0, 'message' => $e->getMessage());
+        }
+    }
+    /**
+     * Obtiene a todos los origenes
+     *    
+     *
+     * @return false|array con información de los usuarios
+     */  
+    public function getOrigenes(string $select = '*') {
+        return $this->db->select($select,'origen');
+    }
+    /**
+     * Eliminar usuario
+    */
+    final public function del() {
+        Global $config;
+       $res = $this->db->delete('origen',"id_origen='$this->id'",'1');
+      # Redireccionar al controlador usuarios con un success=true
+      Functions::redir($config['build']['url'] . 'origenes/&success=true');
+    }
     /**
      * __construct()
     */
     public function __construct(IRouter $router = null) {
         parent::__construct($router);
-		$this->startDBConexion();
+        $this->startDBConexion();
     }
 }
