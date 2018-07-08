b53fce0b73285f93c3d3565568bad5c7096de0c5
Alexander De Azevedo <alexander_deazevedo@hotmail.com>
2018-07-08 19:41:43 -0400

    colocando el sweetalert

--- framework/views/propios/js/delete_item.js
+++ framework/views/propios/js/delete_item.js
@@ -5,5 +5,15 @@
  * @param {*} controlador 
  */
 function delete_item(id_elemento, controlador) {
-    toastr.warning('¡El elemento fue eliminado con éxito!', 'ELIMINACIÓN');
+    swal({
+        title: "¿Estás seguro de eliminar este elemento?",
+        text: "Una vez eliminado no podrás recuperarlo",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "¡Si, quiero eliminar!",
+        closeOnConfirm: false
+    }, function () {
+        swal("Eliminado!", "Tu archivo fue eliminado, exitosamente", "success");
+    });
 }
