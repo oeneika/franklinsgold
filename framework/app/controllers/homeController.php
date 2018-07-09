49ee1c77a97e7f3e3607b6b7c89e51b221d98962
Sergio García <sagg_301@outlook.com>
2018-07-09 10:31:56 -0400

    Graficas de plata y oro funcional

--- framework/app/controllers/homeController.php
+++ framework/app/controllers/homeController.php
@@ -25,9 +25,9 @@ use Ocrend\Kernel\Router\IRouter;
 class homeController extends Controllers implements IControllers {
 
     public function __construct(IRouter $router) {
-        parent::__construct($router,array(
+        parent::__construct($router/*,array(
             'users_logged' => true
-        ));
+        )*/);
         $this->template->display('home/home');
     }
 }
---
+++ framework/assets/jscontrollers/home/dashboard.js
@@ -0,0 +1,120 @@
+$(document).ready(function() {
+    var lineData;
+    $.ajax({
+        type : "GET",
+        url : "https://www.quandl.com/api/v3/datasets/LBMA/SILVER.json", //Json con el precio de la plata
+        dataType: 'json',
+        success : function(json) {
+            var label=[], data=[];
+            var dias = 10;
+            
+            //Se obtienen los datos necesarios de los ultimos dias definidos
+            for(var i=0; i<= dias; i++){
+                label[i]=json.dataset.data[dias-i][0];
+                data[i]=json.dataset.data[dias-i][1].toFixed(2);
+            }
+            
+            lineData = {
+                labels: label,
+                datasets: [
+                    {
+                        label: "Plata",
+                        fillColor: "rgba(221, 175, 5,0.5)",
+                        strokeColor: "rgba(221, 175, 5,1)",
+                        pointColor: "rgba(221, 175, 5,1)",
+                        pointStrokeColor: "#fff",
+                        pointHighlightFill: "#fff",
+                        pointHighlightStroke: "rgba(221, 175, 5,1)",
+                        data: data
+                    }
+                ]
+            };
+
+            var lineOptions = {
+                scaleShowGridLines: true,
+                scaleGridLineColor: "rgba(0,0,0,.05)",
+                scaleGridLineWidth: 1,
+                bezierCurve: true,
+                bezierCurveTension: 0.4,
+                pointDot: true,
+                pointDotRadius: 4,
+                pointDotStrokeWidth: 1,
+                pointHitDetectionRadius: 20,
+                datasetStroke: true,
+                datasetStrokeWidth: 2,
+                datasetFill: true,
+                responsive: true,
+            };
+
+
+            var ctx = document.getElementById("plataChart").getContext("2d");
+            var myNewChart = new Chart(ctx).Line(lineData, lineOptions);
+        },
+        error : function(xhr, status) {
+            alert('Ha ocurrido un problema interno');
+        },
+        complete: function(){ 
+        } 
+    });
+
+    $(document).ready(function() {
+        var lineData
+        $.ajax({
+            type : "GET",
+            url : "https://www.quandl.com/api/v3/datasets/LBMA/GOLD.json",
+            dataType: 'json',
+            success : function(json) {
+                var label=[], data=[];
+                var dias = 10;
+                
+                for(var i=0; i<= dias; i++){
+                    label[i]=json.dataset.data[dias-i][0];
+                    data[i]=json.dataset.data[dias-i][1];
+                }
+                
+                lineData = {
+                    labels: label,
+                    datasets: [
+                        {
+                            label: "Oro",
+                            fillColor: "rgba(221, 175, 5,0.5)",
+                            strokeColor: "rgba(221, 175, 5,1)",
+                            pointColor: "rgba(221, 175, 5,1)",
+                            pointStrokeColor: "#fff",
+                            pointHighlightFill: "#fff",
+                            pointHighlightStroke: "rgba(221, 175, 5,1)",
+                            data: data
+                        }
+                    ]
+                };
+
+                var lineOptions = {
+                    scaleShowGridLines: true,
+                    scaleGridLineColor: "rgba(0,0,0,.05)",
+                    scaleGridLineWidth: 1,
+                    bezierCurve: true,
+                    bezierCurveTension: 0.4,
+                    pointDot: true,
+                    pointDotRadius: 4,
+                    pointDotStrokeWidth: 1,
+                    pointHitDetectionRadius: 20,
+                    datasetStroke: true,
+                    datasetStrokeWidth: 2,
+                    datasetFill: true,
+                    responsive: true,
+                };
+    
+    
+                var ctx = document.getElementById("lineChart").getContext("2d");
+                var myNewChart = new Chart(ctx).Line(lineData, lineOptions);
+            },
+            error : function(xhr, status) {
+                alert('Ha ocurrido un problema interno');
+            },
+            complete: function(){ 
+            } 
+        });
+
+    });
+
+});
