7e383e9345e5ce4e0071ba29f1547ce4cc7fedd8
Alexander De Azevedo <alexander_deazevedo@hotmail.com>
2018-08-29 19:31:26 -0400

    arreglando js de crear ordenes

--- framework/assets/jscontrollers/ordenes/crear.js
+++ framework/assets/jscontrollers/ordenes/crear.js
@@ -15,7 +15,6 @@ function showModal(ultimo_precio_oro,ultimo_precio_plata,precio_BsS) {
     this.ultimo_precio_oro = ultimo_precio_oro/28.3495;
     this.ultimo_precio_plata = ultimo_precio_plata/28.3495;
     this.precio_bolivar_soberano = precio_BsS;
-
 }
 
 
