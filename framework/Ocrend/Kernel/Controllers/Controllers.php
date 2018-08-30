305bd3a27041aa8e3b171a8775874ebfa5e3c583
Greg Gomez <g.a95@hotmail.com>
2018-08-29 21:09:30 -0400

    fix landing

--- framework/Ocrend/Kernel/Controllers/Controllers.php
+++ framework/Ocrend/Kernel/Controllers/Controllers.php
@@ -161,7 +161,7 @@ abstract class Controllers {
 
       # Sólamente usuarios logeados
       if ($this->controllerConfig['users_logged'] && !$this->is_logged) {
-        Helper\Functions::redir($config['build']['url'] . 'login');
+        Helper\Functions::redir($config['build']['url'] . 'landing/');
       }
 
       # Sólamente usuarios tipo admin
