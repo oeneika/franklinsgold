fd9434c9ee0a2371625171668d72ef4063828f71
Alexander De Azevedo <alexander_deazevedo@hotmail.com>
2018-09-22 21:49:25 -0400

    corrigiendo peo de las notificaciones

--- framework/assets/jscontrollers/usuarios/notificaciones.js
+++ framework/assets/jscontrollers/usuarios/notificaciones.js
@@ -4,7 +4,8 @@
  */
 function mark_as_read(id_notificacion) {
 
-    $('#mensaje_'+id_notificacion).css("background-color", "#f8fafb");
+    $('#mensaje_'+id_notificacion).css("background-color", "#e7e7e7");
+    $('#boto_'+id_notificacion).css("color", "#e7e7e7");
     $('#boton_'+id_notificacion).attr('disabled','disabled');
 
     $.ajax({
